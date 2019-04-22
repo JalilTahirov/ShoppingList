a5834605225b5fb48a17d26107c526d00fba8f5b
Jalil Tahirov <jalil.tahirov@outlook.com>
2019-04-22 09:25:14 +0400

    controller test added

---
+++ test/Shoppinglist.Test/ProductsControllerFixture.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shoppinglist.API.Controllers;
+using Shoppinglist.API.Services;
+using System.Threading.Tasks;
+
+namespace Shoppinglist.Test
+{
+    [TestClass]
+    public class ProductsControllerFixture
+    {
+        [TestInitialize]
+        public void OnTestInitialize()
+        {
+            _SystemUnderTest = null;
+        }
+
+        private ProductsController _SystemUnderTest;
+        public ProductsController SystemUnderTest
+        {
+            get
+            {
+                if (_SystemUnderTest == null)
+                {
+                    _SystemUnderTest = new ProductsController(new ProductService(), null);
+                }
+
+                return _SystemUnderTest;
+            }
+        }
+
+
+        [TestMethod]
+        public async void ProductsController_GetAllAsync_ModelIsNotNull()
+        {
+            var actual = await SystemUnderTest.GetAllAsync();
+            //    UnitTestUtility.GetModel<CalculatorViewModel>(
+            //        SystemUnderTest.Index());
+
+            Assert.IsNotNull(actual, "Model was null.");
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // arrange
+
+
+            // act
+
+
+            // assert
+        }
+
+
+    }
+}
